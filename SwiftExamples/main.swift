945342cf9a8d8b4fb4f6fec589356526b2e8d673
Vadim A <id.vadim.a@gmail.com>
2023-04-10 15:21:37 +0300

    Create Project

--- SwiftExamples/main.swift
+++ SwiftExamples/main.swift
@@ -1,11 +1,7 @@
-//
-//  main.swift
-//  SwiftExamples
-//
-//  Created by en.dev on 10.04.2023.
-//
-
 import Foundation
 
-print("Hello, World!")
-
+func sayHello() {
+  print("Hello, world!")
+}
+ 
+sayHello()
