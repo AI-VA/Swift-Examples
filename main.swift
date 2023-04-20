d2bf8f06c921ed1944d4689dfa555193d1dfce13
Vadim A <id.vadim.a@gmail.com>
2023-04-20 14:46:12 +0300

    Add lesson 3, 4, 5

--- main.swift
+++ main.swift
@@ -1,17 +1,3 @@
 import Foundation
 
 
-// MARK: 4. Functional types
-func isEven(number: Int) -> Bool {
-    number % 2 == 0
-}
-
-// Now let's write a function that will take the isEven function as a parameter and convert its result into the String type:
-
-func generateNumberString(number: Int, checkFunction: (Int) -> Bool) -> String {
-  let isEven = checkFunction(number)       // Passed the function as a parameter isEven(5)
-  return isEven ? "Чётное" : "Нечётное"
-}
- 
-let string = generateNumberString(number: 5, checkFunction: isEven)
-print(string)
