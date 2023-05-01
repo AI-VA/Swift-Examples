e45d1fd6f579df0fbb6792f6554ddbcf1fb0d63f
Vadim A <id.vadim.a@gmail.com>
2023-05-01 16:12:03 +0300

    Initial commit

--- main.swift
+++ main.swift
@@ -1,31 +1,3 @@
 import Foundation
 
 
-enum ArithmeticExpressions {
-    case addition (Float, Float)
-    case subtraction (Float, Float)
-    case multiplication (Float, Float)
-    case division (Float, Float)
-    
-    func computation (arithmetic: ArithmeticExpressions) -> Float {
-        switch arithmetic {
-        case let.addition(numberOne, numberTwo):
-            return numberOne + numberTwo
-        case let.subtraction(numberOne, numberTwo):
-            return numberOne - numberTwo
-        case let.multiplication(numberOne, numberTwo):
-            return numberOne * numberTwo
-        case let.division(numberOne, numberTwo):
-            return numberOne / numberTwo
-        }
-    }
-}
-
-let addition = ArithmeticExpressions.addition(3, 4)
-print(addition.computation(arithmetic: addition))
-let subtraction = ArithmeticExpressions.subtraction(3, 4)
-print(addition.computation (arithmetic: subtraction))
-let multiplication = ArithmeticExpressions.multiplication(3, 4)
-print(addition.computation (arithmetic: multiplication))
-let division = ArithmeticExpressions.division(3, 4)
-print(addition.computation(arithmetic: division))
